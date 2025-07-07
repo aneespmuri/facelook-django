0d0abc3efbcfe97313e9a8c0d7a9a448e6c52a52
manojhidragen12 <manoj.hidragen@gmail.com>
2025-07-07 16:49:42 +0530

    addind env

--- black_scissors/settings.py
+++ black_scissors/settings.py
@@ -28,6 +28,10 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+
+]
 
 # Application definition
 
@@ -55,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'black_scissors.urls'
